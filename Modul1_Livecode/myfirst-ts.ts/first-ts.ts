5c2c901b28b909acd0ea7a291734c9f50ebfaa23
lisayl1688 <lisa.y.ly1688@gmail.com>
2024-05-01 18:38:31 +0200

    mandatory push

---
+++ Modul1_Livecode/myfirst-ts.ts/first-ts.ts
@@ -0,0 +1,5 @@
+const firstname: string ='suzy';
+const birthyear: number= 1992;
+const currentYear: number =2024;
+
+console.log('${firstname} will be ${currentYear - birthyear} in $ {currentYear}');
